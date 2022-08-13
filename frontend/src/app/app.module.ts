eb1c7cd7cb526a04c080586252ff7cefbdfaadc2
sr155 <104543440+sr155@users.noreply.github.com>
2022-08-13 11:42:17 +0530

    created components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { MembersComponent } from './members/members.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent,
+    HomeComponent,
+    MembersComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
